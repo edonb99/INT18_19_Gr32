0bcc45212caad6d2f0313f3d21c1d7eb294e00e6
Edon Budakova <ebudakova99@gmail.com>
2019-01-02 18:48:26 +0100

    Math Fun
    funksione JS math te events

---
+++ mathematics.js
@@ -0,0 +1,30 @@
+/* Detyra 1 */
+
+function Detyra_1()
+{
+document.getElementById("detyra1").innerHTML=Math.round(4889.75999);
+}
+
+/* --- */
+
+/* Detyra 2  */
+function RandomNr()
+{
+document.getElementById("random").innerHTML=Math.random();
+}
+/* -------- */
+
+
+/* Detyra 3 */
+    function Detyra_3()
+{
+document.getElementById("sqrt").innerHTML = Math.sqrt(625);
+}
+/* --------- */
+
+/* Detyra 4 */
+    function Detyra_4() 
+{
+document.getElementById("min").innerHTML = Math.min(230, -1.5, -125.5, 10, -2, -125.52);
+}
+/* ------------- */
