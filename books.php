50ba74553b020835a003eeb1e1803f23c520476b
FitimHajredini59 <44604433+FitimHajredini59@users.noreply.github.com>
2019-05-28 00:19:17 +0200

    shtimi i books.php

---
+++ books.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Books</title>
+    <meta charset="utf-8"/>
+</head>    
+
+<body>
+    
+<?php
+    include 'header.php';
+    
+    echo "<br/><br/><br/><br/><br/><br/>";
+    echo "<br/><br/><br/><br/><br/><br/>";
+    echo "<br/><br/><br/><br/><br/><br/>";
+    echo "<br/><br/><br/><br/><br/><br/>";
+    echo "<br/><br/><br/><br/><br/><br/>";
+    echo "<br/><br/><br/><br/><br/><br/>";
+?>    
+    
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book1.jpg" width="250px" height="375px" />
+      </div>
+      <h2>Enchantment</h2>
+      <p class="author">by Guy Kawasaki</p>
+    </figure>
+
+    <br/><br/><br/>
+    
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book2.jpg" width="250px" height="375px" />
+      </div>
+      <h2>CITY</h2>
+      <p class="author">by P.D.Smith</p>
+    </figure>
+
+    <br/><br/><br/>
+    
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book3.jpg" width="250px" height="375px" />
+      </div>
+      <h2>Just Courage</h2>
+      <p class="author">by Gary A.Haugen</p>
+    </figure>
+    
+    <br/><br/><br/>   
+    
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book4.jpg" width="250px" height="375px" />
+      </div>
+      <h2>The Tattooist of Auschwitz</h2>
+      <p class="author">by Heather Morris</p>
+    </figure>
+    
+    <br/><br/><br/>   
+    
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book5.jpg" width="250px" height="375px" />
+      </div>
+      <h2>Harry Potter and the Philosopher's Stone</h2>
+      <p class="author">by J.K. Rowling</p>
+    </figure>
+<?php
+    echo "<br/><br/><br/><br/><br/><br/>";
+    echo "<br/><br/><br/><br/><br/><br/>";
+    echo "<br/><br/><br/><br/><br/><br/>";
+    echo "<br/><br/><br/><br/><br/><br/>";
+    echo "<br/><br/><br/><br/><br/><br/>";
+    echo "<br/><br/><br/><br/><br/><br/>";
+
+    include 'footer.php';
+?>
+</body>    
+</html>
