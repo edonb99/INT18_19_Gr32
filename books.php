78e6b03cf024847bfccb6d65e2851dbb38516079
FitimHajredini59 <44604433+FitimHajredini59@users.noreply.github.com>
2019-05-28 17:12:07 +0200

    shtimi i librave

--- books.php
+++ books.php
@@ -3,77 +3,116 @@
 <head>
     <title>Books</title>
     <meta charset="utf-8"/>
-</head>    
+    <link rel="stylesheet" type="text/css" href="footer.css"/>
+    <link rel="stylesheet" type="text/css" href="books.css"/>
+</head>
 
 <body>
-    
-<?php
-    include 'header.php';
-    
-    echo "<br/><br/><br/><br/><br/><br/>";
-    echo "<br/><br/><br/><br/><br/><br/>";
-    echo "<br/><br/><br/><br/><br/><br/>";
-    echo "<br/><br/><br/><br/><br/><br/>";
-    echo "<br/><br/><br/><br/><br/><br/>";
-    echo "<br/><br/><br/><br/><br/><br/>";
-?>    
-    
+<div style="background-color: white">
+
+  <br><br>
+
     <figure class="thumb">
       <div class="cover">
-        <img src="Images/Book1.jpg" width="250px" height="375px" />
+        <img src="Images/Book1.jpg"/>
       </div>
       <h2>Enchantment</h2>
       <p class="author">by Guy Kawasaki</p>
     </figure>
 
-    <br/><br/><br/>
-    
     <figure class="thumb">
       <div class="cover">
-        <img src="Images/Book2.jpg" width="250px" height="375px" />
+        <img src="Images/Book2.jpg"/>
       </div>
       <h2>CITY</h2>
       <p class="author">by P.D.Smith</p>
     </figure>
 
-    <br/><br/><br/>
-    
     <figure class="thumb">
       <div class="cover">
-        <img src="Images/Book3.jpg" width="250px" height="375px" />
+        <img src="Images/Book3.jpg"/>
       </div>
       <h2>Just Courage</h2>
       <p class="author">by Gary A.Haugen</p>
     </figure>
-    
-    <br/><br/><br/>   
-    
+
     <figure class="thumb">
       <div class="cover">
-        <img src="Images/Book4.jpg" width="250px" height="375px" />
+        <img src="Images/Book4.jpg"/>
       </div>
       <h2>The Tattooist of Auschwitz</h2>
       <p class="author">by Heather Morris</p>
     </figure>
-    
-    <br/><br/><br/>   
-    
+
     <figure class="thumb">
       <div class="cover">
-        <img src="Images/Book5.jpg" width="250px" height="375px" />
+        <img src="Images/Book5.jpg"/>
       </div>
-      <h2>Harry Potter and the Philosopher's Stone</h2>
+      <h2>Harry Potter</h2>
       <p class="author">by J.K. Rowling</p>
     </figure>
+
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book6.jpg"/>
+      </div>
+      <h2>Every Breath</h2>
+      <p class="author">by Nicolas Sparks</p>
+    </figure>
+
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book7.jpg"/>
+      </div>
+      <h2>The lost girls of Paris</h2>
+      <p class="author">by Pan Jenoff</p>
+    </figure>
+
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book8.jpg"/>
+      </div>
+      <h2>The Reckoning</h2>
+      <p class="author">by John Grisham</p>
+    </figure>
+
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book9.jpg"/>
+      </div>
+      <h2>The couple next door</h2>
+      <p class="author">by Shari Lapena</p>
+    </figure>
+
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book10.jpg"/>
+      </div>
+      <h2>The silent patient</h2>
+      <p class="author">by Alex Michaelides</p>
+    </figure>
+
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book11.jpg"/>
+      </div>
+      <h2>Where the cowards sing</h2>
+      <p class="author">by Delia Owens</p>
+    </figure>
+
+    <figure class="thumb">
+      <div class="cover">
+        <img src="Images/Book12.jpg"/>
+      </div>
+      <h2>The Coin</h2>
+      <p class="author">by Sandeep Sharma</p>
+    </figure>
+
 <?php
-    echo "<br/><br/><br/><br/><br/><br/>";
-    echo "<br/><br/><br/><br/><br/><br/>";
-    echo "<br/><br/><br/><br/><br/><br/>";
-    echo "<br/><br/><br/><br/><br/><br/>";
-    echo "<br/><br/><br/><br/><br/><br/>";
-    echo "<br/><br/><br/><br/><br/><br/>";
+    echo "<br/><br/><br/><br/><br/>";
 
     include 'footer.php';
 ?>
-</body>    
+</div>
+</body>
 </html>
