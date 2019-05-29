0ed7ab0cce5ffe7ac20a677f7fdf9f65d4525418
Blerina Fejza <Blerinafejza98@gmail.com>
2019-05-29 18:39:21 +0200

    .

--- books.php
+++ books.php
@@ -37,7 +37,7 @@
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
     	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    	<p> <a href="books.php?logout='1'" style="color: red;">logout</a> </p>
     <?php endif ?>
 
   <br><br>
