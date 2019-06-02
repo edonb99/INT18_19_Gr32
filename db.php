c2d40f8a14d4807f30cfb3f6d20581f64010914d
Edon Budakova <ebudakova99@gmail.com>
2019-06-02 15:23:42 +0200

    exception

--- db.php
+++ db.php
@@ -1,8 +1,14 @@
 <?php
+
+try{
 $servername = 'localhost';
 $user = 'root';
 $pass = '1234';
 $name = 'ecoschools';
 $db = mysqli_connect($servername,$user,$pass,$name);
+}
 
+catch (Exception $e){
+	echo $e->errorMessage();
+}
 ?>
